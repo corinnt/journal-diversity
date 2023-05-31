e21cf21b436a3397dd98f81f02daba538df0beb5
corinnt <94402869+corinnt@users.noreply.github.com>
2023-05-30 20:03:24 -0700

    basic mapping functionality

--- code/gesta-diversity.py
+++ code/gesta-diversity.py
@@ -2,6 +2,9 @@ from operator import truediv
 from readline import insert_text
 import requests
 import pandas as pd
+import geopandas as gpd
+
+
 
 from mailto import email # just a function that returns your email address
 from map import map_points
@@ -11,6 +14,9 @@ class Data():
         self.titles = []
         self.authors = []
         self.years = []
+
+        self.latitudes = []
+        self.longitudes = []
 
         self.countries = {}
         self.institutions = {}
@@ -22,7 +28,7 @@ def main(source_id):
     source_query = "https://api.openalex.org/sources/" + source_id
     data = Data()
     iterate_search(source_id, data)
-    process_data(data)    
+    display_data(data)    
     # TODO: make protocol for what fields to query?
     # ie get institution ID, else get institution name and country, else get author name and country and year?
 
@@ -70,14 +76,36 @@ def parse_authorship(authorships, data):
     """
     author_string = ""
     for authorship in authorships:
-        author_string += authorship['author']['display_name'] + " "
+        author_string += authorship['author']['display_name'] + "\n"
         for institution in authorship['institutions']:
             code = institution['country_code']
             id = institution['id']
+            if id: parse_geodata(id, data)
             increment(code, data.countries)
             increment(id, data.institutions)
             data.institution_ids.add(id)
     data.authors.append(author_string)
+
+def parse_geodata(id, data):
+    """
+        :param id: - string ID of Institution to check for geodata
+        :param data: - Data object
+    """
+    url =  "https://api.openalex.org/institutions/" + id
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+        results = response.json()
+        if 'latitude' in results['geo']:
+            print("got latitude")
+            lat = results['geo']['latitude']
+            long = results['geo']['longitude']
+            data.latitudes.append(lat)
+            data.longitudes.append(long)
+    except requests.exceptions.RequestException as e:
+        print("Error occurred:", e)
+    except ValueError as e:
+        print("Error decoding JSON:", e)
 
 def iterate_search(source_id, data):
     """
@@ -86,8 +114,7 @@ def iterate_search(source_id, data):
     """
     # only get name and authors for after 1999
     works_query_with_page = 'https://api.openalex.org/works?select=display_name,authorships,concepts,publication_year&filter=publication_year:>1999,locations.source.id:' + source_id + '&page={}&mailto=' + email()
-    # get all Work info after 1999
-    #works_query_with_page = 'https://api.openalex.org/works?select=display_name,authorships&page={}'
+    
     page = 1
     has_more_pages = True
     fewer_than_10k_results = True
@@ -113,23 +140,22 @@ def iterate_search(source_id, data):
         has_more_pages = len(results) == page_size
         fewer_than_10k_results = page_size * page <= 10000
 
-def process_data(data):
+def display_data(data):
     """
         :param data: Data object
     """
-    for title in data.titles:
-        if title != "Front Matter" and\
-            title != "Back Matter" and\
-            title != "Front Cover" and\
-            title != "Back Cover":
-            pass
     #print(data.institutions)
     dict = {'author' : data.authors, 'title' : data.titles, 'year' : data.years}
-    #print(dict)
     df = pd.DataFrame(dict)
     print(df.head())
-    df.to_csv("../data.csv")
-    map_points([], '')
+    #df.to_csv("../data/data.csv")
+
+    map_dict = {'latitude' : data.latitudes, 'longitude' : data.longitudes}
+    map_df = pd.DataFrame(map_dict)
+    global_map = gpd.read_file('../shapefiles/world-map/WB_Land_10m.shp')
+    map_points(map_df, global_map, 'world')
+    us_map = gpd.read_file('../shapefiles/us-map/tl_2012_us_state.shp')
+    map_points(map_df, us_map, 'US')
 
 if __name__ == "__main__":
     gesta_id = "S21591069"
--- code/map.py
+++ code/map.py
@@ -6,24 +6,21 @@ from shapely.geometry import Point, Polygon
 
 
 
-def map_points(points, csv_path): 
+def map_points(df, map, name): 
     """
         :param points: list of tuples of latitudes and longitudes
         :param csv_path: path to csv with data to map
     """ 
-    map = gpd.read_file('../world-map-shapefile/WB_Land_10m.shp')
-    geometry = [Point(xy) for xy in points]
+    geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])]
     fig, ax = plt.subplots(figsize=(15,15))
-    map.plot(ax=ax)
-    plt.savefig('world.jpg')
-
-    """
-       df = pd.read_csv(csv_path)
+   
     crs = {'init':'epsg:4326'}
-    geo_df = gpd.GeoDataFrame(df, #specify our data
-                          crs=crs #specify our coordinate reference system
-                          geometry=geometry) #specify the geometry list we created
-    
-    """
- 
-    #geo_df.head()
+    geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry) #specify the geometry list we created
+    map.plot(ax=ax, color='grey', alpha=0.4)
+    geo_df.plot(ax=ax, 
+                aspect=1,
+                markersize=10, 
+                color='blue', 
+                marker='o')
+    plt.savefig("../data/" + name + ".jpg")
+    geo_df.head()
