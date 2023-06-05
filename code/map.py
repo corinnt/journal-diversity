6dc0624a95e428d7e9fcf50c342037ddf28f71cf
corinnt <94402869+corinnt@users.noreply.github.com>
2023-06-05 00:54:51 -0700

    fixed map bounds wrt to data points

--- code/map.py
+++ code/map.py
@@ -1,38 +1,12 @@
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#import descartes
-#import geopandas as gpd
-#from shapely.geometry import Point, Polygon
 import pygmt
 
-"""
-def map_points(df, map, name): 
-    # 
-        #:param df: DataFrame with longitude and latitude data for points
-        #:param map: GeoPandas
-    # 
-    geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])] # NOTE: swapped order for GeoPandas
-    fig, ax = plt.subplots(figsize=(15,15))
-    
-    crs = {'init':'epsg:4326'}
-    geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry) #specify the geometry list we created
-    map.plot(ax=ax, color='grey', alpha=0.4)
-    geo_df.plot(ax=ax, 
-                aspect=1,
-                markersize=3, 
-                color='blue', 
-                marker='o')
-    plt.savefig("../data/" + name + ".jpg")
-    geo_df.head()
-"""
-def map_points(df, map, name): 
+def map_points(df, name): 
     region = [
         df.longitude.min() - 1,
         df.longitude.max() + 1,
-        0, #df.latitude.min() - 1,
-        65 # df.latitude.max() + 1,
+        df.latitude.min() - 5,
+        df.latitude.max() + 10,
         ]
-    print(region)
     print(df.head())
 
     fig = pygmt.Figure()
